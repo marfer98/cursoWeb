6525623b3dee633aa02f5f088c2873ea052faff8
Marcos Fernandez <marferantonio@gmail.com>
2022-09-09 12:08:27 -0400

    operadores de asignacion

--- JavaScript/leccion3/leccion3.js
+++ JavaScript/leccion3/leccion3.js
@@ -18,3 +18,40 @@ console.log("El resultado del modulo es: "+ z);
 
 z=a**b;
 console.log("El resultado de la exponente es: "+ z);
+
+//incremento
+//pre-incremento (operador ++ antes de la variable)
+z= ++a;
+console.log(z);
+
+
+//post-incremento (operador ++ despues de la variable)
+z= b++;
+console.log(z);
+console.log(b); //aumenta en la segunda vez que se utiliza
+
+//decremento
+//predecremento
+z= --a;
+console.log(a);
+console.log(z);
+//post-decremento
+z=b--;
+console.log(b);
+console.log(z);
+
+let x = 3;
+let y = 2;
+let w = 1;
+let s = 4
+let m = x*y+w /s;
+console.log(m)
+
+m=(w+x)*y/s;
+console.log(m);
+//OPERADORES DE ASIGNACIÓN
+s+=1
+console.log(s); //s= s+1
+
+s-=1
+console.log(s); 
