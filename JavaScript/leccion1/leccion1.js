54a1d87dad95b1b77de77954b757fe8e224b6d65
Marcos Fernandez <marferantonio@gmail.com>
2022-09-09 11:13:45 -0400

    operadores en js

--- JavaScript/leccion1/leccion1.js
+++ JavaScript/leccion1/leccion1.js
@@ -8,4 +8,5 @@ console.log (nombreCompleto);
 let x, y;
 x=20, y= 34;
 let z= x+y;
-console.log (z)¿
+console.log (z);
+//el nombre de las variables no deben empezar con un numero y tampoco llevar nombre de palabras reservadas
---
+++ JavaScript/leccion3/leccion3.js
@@ -0,0 +1,20 @@
+//operadores JS
+let a =3;
+let b= 2;
+let z= a+b;
+console.log("El resultado de la suma es: "+ z);
+
+z=a-b;
+console.log("El resultado de la resta es: "+ z);
+
+z=a*b;
+console.log("El resultado de la multiplicación es: "+ z);
+
+z=a/b;
+console.log("El resultado de la división es: "+ z);
+
+z=a%b;
+console.log("El resultado del modulo es: "+ z);
+
+z=a**b;
+console.log("El resultado de la exponente es: "+ z);
