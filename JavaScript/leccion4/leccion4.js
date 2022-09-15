553ccabb1efae94ef6ed4c3f55a7d4f903ed45c7
Marcos Fernandez <marferantonio@gmail.com>
2022-09-15 09:30:58 -0400

    if/else

---
+++ JavaScript/leccion4/leccion4.js
@@ -0,0 +1,21 @@
+let condicion = true;
+
+if (condicion){
+    console.log("Verdadero");
+}else{
+    console.log("falso");
+}
+
+let numero = 5;
+
+if (numero ==1){
+    console.log("numero 1");
+}else if(numero == 2){
+    console.log("numero 2");
+}else if (numero == 3){
+    console.log ("es tre");
+}else if (numero == 4){
+    console.log ("es 4");
+}else{
+    console.log("numero desconocido")
+}
